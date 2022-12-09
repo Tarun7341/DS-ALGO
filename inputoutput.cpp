33df2f6d5fb2fc1439d3cb2db31d6c79deb659ee
tarun <agasartarun987@gmail.com>
2022-12-09 17:51:45 +0530

    Day 2

---
+++ inputoutput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int amt1,amt2,sum;
+    cout<<"Enter the amount";
+    cin>>amt1;
+    cout<<"enter the amount";
+    cin>>amt2;
+    sum = amt1+amt2;
+    cout<<"sum of amt1 and amt2 is "<<sum;
+
+}
